13204a9b034d88edbda0ceb30b962652ec4e9752
makar42pro <glaksto@gmail.com>
2025-11-20 08:54:43 +0300

    Update auth.py

--- project-root/auth-module/auth.py
+++ project-root/auth-module/auth.py
@@ -1,7 +1,6 @@
 import data_store
 
 def register():
-    """Регистрация нового пользователя (FR-A1, FR-A2, FR-A3)"""
     print("\n--- Регистрация ---")
     username = input("Введите логин: ")
     if username in data_store.USERS_DB:
@@ -10,12 +9,10 @@ def register():
 
     email = input("Введите email: ")
     password = input("Введите пароль: ")
-    # В реальном приложении пароль нужно хешировать (FR-A4), здесь упрощено
     data_store.USERS_DB[username] = {'password': password, 'email': email}
     print(f"Пользователь '{username}' успешно зарегистрирован!")
 
 def login():
-    """Авторизация пользователя (FR-A5, FR-A6, FR-A7, FR-A8)"""
     print("\n--- Авторизация ---")
     if data_store.CURRENT_USER:
         print(f"Вы уже вошли как {data_store.CURRENT_USER}.")
@@ -27,16 +24,15 @@ def login():
     user_data = data_store.USERS_DB.get(username)
 
     if user_data and user_data['password'] == password:
-        data_store.CURRENT_USER = username # Обновляем глобальное состояние
+        data_store.CURRENT_USER = username 
         print(f"Добро пожаловать, {data_store.CURRENT_USER}! Вы успешно вошли.")
     else:
         print("Ошибка входа: Неверный логин или пароль.")
 
 def logout():
-    """Выход из системы (FR-A9)"""
     if data_store.CURRENT_USER:
         print(f"Пользователь {data_store.CURRENT_USER} вышел из системы.")
-        data_store.CURRENT_USER = None # Обнуляем глобальное состояние
+        data_store.CURRENT_USER = None
     else:
         print("Вы не авторизованы.")
 
